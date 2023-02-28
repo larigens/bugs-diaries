44cabc5b11bb80c6babf717c8f4748d27ee5a0f5
larigens@gmail.com <larigens@gmail.com>
2023-02-28 14:31:26 -0500

    changed style to look good on multiple screen sizes

--- controllers/api/posts-routes.js
+++ controllers/api/posts-routes.js
@@ -1,33 +1,37 @@
 const router = require('express').Router();
 const { Post, PostDiary } = require('../../models');
 
+// Creates a new post.
 router.post('/newpost', async (req, res) => {
   try {
+    // Use Sequelize's `create()` method to add a row to the table.
     const newPost = await Post.create({
       title: req.body.title,
       content: req.body.content,
       user_id: req.session.user_id,
       diaryIds: [],
     });
-
+    // Stores the diaries ids and the new post id inside a variable.
     const postDiaryArr = req.body.diariesIds.map((diary_id) => {
       return {
         post_id: newPost.id,
         diary_id,
       };
     });
-
+    // Use Sequelize's `bulkCreate()` method to add the new data to the post_diary table.
     await PostDiary.bulkCreate(postDiaryArr);
-
     res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+// Updates a post by its `id` value.
 router.put('/editpost', async (req, res) => {
   try {
+    // Calls the update method on the Post model.
     const updatedPost = await Post.update(
+      // All the fields you can update and the data attached to the request body.
       {
         title: req.body.title,
         content: req.body.content,
